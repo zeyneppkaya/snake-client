9aea81763412888b8e28f9be139a77e4085659db
Zeynep <zeynepkaya_26@hotmail.com>
2021-12-01 23:59:21 +0000

    snake multiplay

--- play.js
+++ play.js
@@ -1,25 +1,4 @@
-const net = require("net");
+//const net = require("net");
+const connect = require("./client");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-   console.log('Connected to server.');
-  });
-
-  conn.on("data", (data) => {
-    console.log('New message from server :', data);
-   });
-
-  return conn;
-};
-
-console.log("Connecting ...");
 connect();
