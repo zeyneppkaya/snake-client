c558e762abb1d6898498f5c4c3aaa3d3462c0f63
Zeynep <zeynepkaya_26@hotmail.com>
2021-12-02 01:34:09 +0000

    snake multiplay

--- play.js
+++ play.js
@@ -1,4 +1,24 @@
-//const net = require("net");
 const connect = require("./client");
 
+// setup interface to handle user input from stdin
+
+const handleUserInput = function (data) {
+    if (data === '\u0003') {
+        stdout.write("Exited the game.\n");
+        process.exit();
+    }
+};
+
+const setupInput = function () {
+    const stdin = process.stdin;
+
+    stdin.setRawMode(true);
+    stdin.setEncoding('utf8');
+    stdin.resume();
+    stdin.on("data", handleUserInput);
+    return stdin;
+};
+
+setupInput();
 connect();
+
